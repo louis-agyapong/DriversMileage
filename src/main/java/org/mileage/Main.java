6fd2c985054a7b8b96051d185d5528e531dfa759
Louis Agyapong <47106199+louis-agyapong@users.noreply.github.com>
2022-10-14 01:38:13 +0000

    Initial commit

---
+++ src/main/java/org/mileage/Main.java
@@ -0,0 +1,42 @@
+/**
+ * QUESTION Drivers are concerned with the mileage their automobiles get. One driver has kept track
+ * of several trips by recording the miles driven and gallons of fuel used. Develop a java
+ * application that shall input the miles driven and gallons used (both as integers) for each trip.
+ * The program should calculate and display the miles per gallon obtained for all trips, stating how
+ * many trips, how many gallons and the mileage. Use the scanner object to obtain the data from the
+ * user.
+ */
+package org.mileage;
+
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+    int trips;
+    int miles;
+    int gallons;
+    int totalMiles = 0, totalGallons = 0;
+
+    // Use Scanner to get user's(driver's) input
+    Scanner input = new Scanner(System.in);
+    // Get drivers total trips
+    System.out.print("Enter your total number of trips ");
+    trips = input.nextInt();
+    for (int count = 1; count <= trips; count++) {
+      // Get miles driven
+      System.out.printf("Enter miles driven for trip %d ", count);
+      miles = input.nextInt();
+      // Get total miles
+      totalMiles += miles;
+      // Get gallons of fuel used
+      System.out.print("Enter gallons of fuel used ");
+      gallons = input.nextInt();
+      // Get total gallons
+      totalGallons += gallons;
+      System.out.println(totalGallons +  totalMiles);
+      System.out.println(count);
+    }
+
+  }
+}
